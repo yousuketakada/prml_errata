58b4424685921053eefcec196e9870b27bfc57f5
Yousuke Takada <yousuketakada@gmail.com>
2016-11-01 02:27:01 +0900

    Add dummy Octave code

---
+++ m/README.txt
@@ -0,0 +1,2 @@
+This directory contains some Octave code implementing numerical algorithms
+discussed in the report.
---
+++ m/f.m
@@ -0,0 +1,25 @@
+## Copyright N/A
+##
+## f.m - an Octave function
+##
+## Written in 2016 by Yousuke Takada <yousuketakada@gmail.com>.
+##
+## To the extent possible under law, the author(s) have dedicated all copyright
+## and related and neighboring rights to this software to the public domain
+## worldwide. This software is distributed without any warranty.
+##
+## You should have received a copy of the CC0 Public Domain Dedication along
+## with this software. If not, see
+## <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+## Usage: y = f(x)
+##
+## Multiplies x by two.
+##
+## Examples:
+##   f(1)
+##   f(eye(3))
+
+function y = f(x)
+  y = 2*x;
+endfunction
